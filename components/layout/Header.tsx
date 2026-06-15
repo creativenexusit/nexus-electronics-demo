4d4548a59a423214183906336a6703f0a9067c5f
creativenexusit <creativenexusit@gmail.com>
2026-06-15 10:58:21 +0600

    zip

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -8,7 +8,7 @@ import {
   Search,
   Menu,
   X,
-  Zap,
+ 
   ChevronDown,
 } from "lucide-react";
 import { useStore } from "@/context/StoreContext";
@@ -43,9 +43,6 @@ export default function Header() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link href="/" className="flex items-center gap-2 font-bold text-xl">
-            <div className="w-8 h-8 bg-blue-600 rounded-lg flex items-center justify-center">
-              <Zap className="w-5 h-5 text-white" />
-            </div>
             <span className="text-blue-600">Nexus</span>
             <span className="text-gray-900">Electronic</span>
           </Link>
